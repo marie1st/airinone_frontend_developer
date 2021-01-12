b7e6c3e80e18bdf002dc6911b441b7f15e42468a
Mariebouquin Sriphanphet <69067303+marie1st@users.noreply.github.com>
2021-01-12 15:28:06 +0700

    Add files via upload

--- src/containers/Shop/Order/Install/INeedApi.js
+++ src/containers/Shop/Order/Install/INeedApi.js
@@ -50,7 +50,6 @@ export const INeedApi = () => {
   const [NewAppointmentData, setAppointment] = useState([{date: "", time_period: "", order_id: "", employee_id: ""}])
   var value = "";
   var resDate = "";
-  var Dateapp = require('../../../../dateformat');
   var post;
   var orderdelete = require('./orderdelete');
   var acceptworkapi = require('./acceptworkapi');
@@ -80,7 +79,6 @@ export const INeedApi = () => {
 
   useEffect(() => {
     fetchData();
-    Dateformat();
   }, [])
 
   return (
